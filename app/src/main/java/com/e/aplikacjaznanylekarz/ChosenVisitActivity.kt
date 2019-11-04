ece49d85bc8f0a43a901f04e76404e7302565d97
Patryk <gustaw5022@gmail.com>
2019-11-04 15:58:26 +0100

    added chosenVisitActivity and made view

---
+++ app/src/main/java/com/e/aplikacjaznanylekarz/ChosenVisitActivity.kt
@@ -0,0 +1,12 @@
+package com.e.aplikacjaznanylekarz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChosenVisitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chosen_visit)
+    }
+}
