d3edf11da0513e48e63a9adcca5ae235ea60b62e
Patryk <gustaw5022@gmail.com>
2019-11-04 15:40:54 +0100

    added visitListActivity and made view

---
+++ app/src/main/java/com/e/aplikacjaznanylekarz/VisitListActivity.kt
@@ -0,0 +1,12 @@
+package com.e.aplikacjaznanylekarz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class VisitListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visit_list)
+    }
+}
